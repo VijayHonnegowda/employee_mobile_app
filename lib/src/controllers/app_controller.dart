e6c508c5cbeda14e75427c019c848451f7f4686b
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-02-12 13:00:53 +0530

    created the language notifier in app controller and updated the common exports file.

--- lib/src/controllers/app_controller.dart
+++ lib/src/controllers/app_controller.dart
@@ -1,6 +1,10 @@
+import 'package:mobile_app_platform_widgets/common_exports.dart';
+
 class AppController {
   factory AppController() => _singleton;
   static final AppController _singleton = AppController._internal();
 
   AppController._internal();
+
+  final ValueNotifier<bool> languageNotifier = ValueNotifier(false);
 }
--- lib/src/utilities/common_exports.dart
+++ lib/src/utilities/common_exports.dart
@@ -1,4 +1,7 @@
 export 'package:flutter/material.dart';
+export 'package:employee_mobile_app/generated/l10n.dart';
 export 'package:mobile_app_platform_widgets/common_exports.dart';
 export 'package:employee_mobile_app/initalize_app_resources.dart';
+export 'package:employee_mobile_app/src/controllers/app_controller.dart';
+export 'package:employee_mobile_app/src/services/navigation_services.dart';
 export 'package:tyrios_mobile_app_flutter_extensions/tyrios_mobile_app_flutter_extensions.dart';
