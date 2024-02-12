d6d23951312fbe80dba2c879d9f7c7b7813af711
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-02-12 12:59:01 +0530

    created the app constant file.

---
+++ lib/src/utilities/app_constants.dart
@@ -0,0 +1,7 @@
+import 'package:employee_mobile_app/generated/l10n.dart';
+import 'package:employee_mobile_app/src/controllers/app_controller.dart';
+
+late S strings;
+// const String kAppName = 'Handball in Biberach';
+final AppController appController = AppController();
+// final LoginController loginController = LoginController();
